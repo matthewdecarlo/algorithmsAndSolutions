d4d9cdcbadd819b60ee55f654745b3403825953b
Matthew DeCarlo <matthewwho@me.com>
2018-12-19 21:05:31 -0600

    update solution to break all items have been compared; rather than wait for entire every operation to iterate over the array

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -6,19 +6,46 @@
 // --- Examples:
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
-const reverse = require('../reversestring/index.js');
 
 // import and use previously written reverse method to find truth
+// const reverse = require('../reversestring/index.js');
+
 // function palindrome(str) {
 //   return str === reverse(str)
 // }
 
 // solve with array methods and compare truthness of each oposing character
-function palindrome(str) {
-  return str.split('').every((character, index) => {
-    const oppisiteCharacter = str.length - index - 1;
+// update solution to break all items have been compared; rather than wait
+// for entire 'every' operation to iterate over the array.
+function isOdd(number) {
+  return number % 2;
+}
 
-    return character === str[oppisiteCharacter];
+function dissectString(string) {
+  const length = string.length;
+  const halfLength = Math.floor(length / 2);
+  const isLengthOdd = isOdd(length);
+  let stringArray = string.split('');
+
+  if (isLengthOdd) {
+    const front = stringArray.splice(0, halfLength);
+    const end = stringArray.slice(1, halfLength + 1);
+    return { front, end };
+  }
+  
+  return {
+    front: stringArray.splice(0, halfLength),
+    end: stringArray };
+}
+
+function palindrome(string) {
+  const dissectedString = dissectString(string);
+  const front = dissectedString.front;
+
+  return front.every((character, index) => {
+    const oppisiteCharacter = front.length - index - 1;
+
+    return character === dissectedString.end[oppisiteCharacter];
   })
 }
 
