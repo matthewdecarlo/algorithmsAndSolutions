d3e9d3ae9aa8e9008e4d6e3de2444b55edc5edd2
Matthew DeCarlo <matthewwho@me.com>
2018-12-20 11:52:43 -0600

    • use reduce method to create an object set mapped to the corresponding character count • use reduce on the key value pairs to filter out the maximum count

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// use reduce method to create an object set mapped to the corresponding
+// character count
+// use reduce on the key value pairs to filter out the maximum count.
+function maxChar(str) {
+  const characterCount = str.split('').reduce((characterCount, character) => {
+    characterCount[character] = ++characterCount[character] || 1;
+
+    return characterCount;
+  }, {} )
+
+  const largestCount = Object.entries(characterCount).reduce((a, b) => {
+    return a[1] > b[1] ? a : b;
+  });
+
+  return largestCount[0];
+}
 
 module.exports = maxChar;
