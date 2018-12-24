e21e0455c35820a1a33b33cc974ef2f8cba0800f
Matthew DeCarlo <matthewwho@me.com>
2018-12-24 13:58:42 -0600

    add method solving with for loop & slice

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,37 +9,47 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 // add solution using reduce with conditionals
-// function chunk(array, size) {
-//   return array.reduce((chunks, value) => {
-//     const length = chunks.length;
-//     const isEmpty = length === 0;
-    
-//     if (isEmpty) return [[value]];
-    
-//     const currentChunk = chunks[length - 1];
-//     const isSmallerThanSize = currentChunk.length < size;
-    
-//     if (isSmallerThanSize) {
-//       currentChunk.push(value)
-//       return chunks;
-//     }
-//     else {
-//       chunks.push([value]);
-//     }
-
-//     return chunks;
-//   }, [])
-// }
-
-// add method solving with splice
 function chunk(array, size) {
-  const chunks = [];
+  return array.reduce((chunks, value) => {
+    const length = chunks.length;
+    const isEmpty = length === 0;
+    
+    if (isEmpty) return [[value]];
+    
+    const currentChunk = chunks[length - 1];
+    const isSmallerThanSize = currentChunk.length < size;
+    
+    if (isSmallerThanSize) {
+      currentChunk.push(value)
+      return chunks;
+    }
+    else {
+      chunks.push([value]);
+    }
 
-  while (array.length) {
-    chunks.push(array.splice(0, size))
-  }
-
-  return chunks;
+    return chunks;
+  }, [])
 }
 
+// add method solving with splice
+// function chunk(array, size) {
+//   const chunks = [];
+
+//   while (array.length) {
+//     chunks.push(array.splice(0, size))
+//   }
+
+//   return chunks;
+// }
+
+// add method solving with for loop & slice
+// function chunk(array, size) {
+//   const chunks = [];
+
+//   for (index = 0, length = array.length; index < length; index += size) {
+//     chunks.push(array.slice(index, index + size));
+//   }
+
+//   return chunks;
+// }
 module.exports = chunk;
